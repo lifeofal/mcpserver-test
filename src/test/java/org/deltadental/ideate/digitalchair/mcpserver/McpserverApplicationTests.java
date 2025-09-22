9cd45c2289bad316fb2f21e3847297f87b8a6508
Alejandro Cervantes <acervan.personal@gmail.com>
2025-09-22 10:00:06 -0400

    First commit

---
+++ src/test/java/org/deltadental/ideate/digitalchair/mcpserver/McpserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deltadental.ideate.digitalchair.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
